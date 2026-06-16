c331a00fa872689dc5608cef9facc864e68cfffe
Hugh Scott <hughscott2002@yahoo.com>
2026-06-16 16:18:30 -0500

    Added bun to the mix

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -5,9 +5,9 @@ import { logError } from "../utils/logger.js";
 const redis =
   env.UPSTASH_REDIS_REST_URL && env.UPSTASH_REDIS_REST_TOKEN
     ? new Redis({
-        url: env.UPSTASH_REDIS_REST_URL,
-        token: env.UPSTASH_REDIS_REST_TOKEN,
-      })
+      url: env.UPSTASH_REDIS_REST_URL,
+      token: env.UPSTASH_REDIS_REST_TOKEN,
+    })
     : null;
 
 function serialize(value: unknown): string {
